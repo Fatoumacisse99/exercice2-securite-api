b27f46d718701ceb60b6ae0c3bd217df21dc80b7
Fatoumacisse99 <cfama673@gmail.com>
2024-10-04 12:31:22 +0200

    Initialisation et ajout du fichier app.js

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';
+
+const app = express();
+const port = 3015;
+
+app.use(helmet());
+
+// Limite les requêtes à 100 toutes les 15 minutes
+const limiter = rateLimit({
+  windowMs: 1* 60 * 1000, 
+  max: 4,
+});
+
+
+app.use(limiter);
+app.use(cors({
+  origin: 'http://example.com', 
+}));
+
+app.get('/api/hello', (_req, res) => {
+  res.json({ message: 'Hello world' });
+});
+
+app.listen(port, () => {
+  console.log(`API running on http://localhost:${port}`);
+});
